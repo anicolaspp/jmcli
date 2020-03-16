c5157e76434e738ddf1f8fccc764b4b0b931f765
Nicolas A Perez <anicolaspp@gmail.com>
2020-03-16 11:23:35 -0400

    table api

--- src/main/scala/com/github/anicolasp/mapr/cli/table/TableEntry.scala
+++ src/main/scala/com/github/anicolasp/mapr/cli/table/TableEntry.scala
@@ -2,10 +2,14 @@ package com.github.anicolasp.mapr.cli.table
 
 import com.github.anicolasp.mapr.cli.client.MapRCLI.Auth
 import com.github.anicolasp.mapr.cli.entries.Entry
-import com.github.anicolasp.mapr.cli.runnable.RunnableCommand
+import com.github.anicolasp.mapr.cli.runnable.{RunnableCommand, RunnableQuery}
 
 trait TableEntry {
   def create(path: String, args: Iterable[(String, String)]): RunnableCommand
+
+  def delete(path: String): RunnableCommand
+
+  def info(path: String): RunnableQuery
 }
 
 object TableEntry {
@@ -14,6 +18,12 @@ object TableEntry {
   class TableE(host: String, auth: Option[Auth]) extends Entry(s"${host}/rest/table") with TableEntry {
     override def create(path: String, args: Iterable[(String, String)]): RunnableCommand =
       RunnableCommand(getUrl("create"), auth, ("path", path) :: args.toList)
+
+    override def delete(path: String): RunnableCommand =
+      RunnableCommand(getUrl("delete"), auth, List(("path", path)))
+
+    override def info(path: String): RunnableQuery =
+      RunnableQuery(getUrl("info"), auth, List(("path", path)))
   }
 
 }
