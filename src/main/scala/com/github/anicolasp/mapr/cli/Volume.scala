1542c803dbaf1f479eac6f6e8dc86acbce626c75
Nicolas A Perez <anicolaspp@gmail.com>
2020-03-16 10:39:50 -0400

    Update Volume.scala

--- src/main/scala/com/github/anicolasp/mapr/cli/Volume.scala
+++ src/main/scala/com/github/anicolasp/mapr/cli/Volume.scala
@@ -8,23 +8,38 @@ trait Volume {
   def list(): RunnableQuery
 
   def create(name: String, args: Iterable[(String, String)] = Iterable.empty): RunnableCommand
+
+  def mount(name: String, path: String): RunnableCommand
+
+  def unmount(name: String, force: Boolean): RunnableCommand
 }
 
 object Volume {
 
   def apply(host: String, auth: Option[Auth]): Volume = new Volume {
-    override def list(): RunnableQuery = {
-      val url = host + "/rest/volume/list"
 
-      RunnableQuery(url, auth)
-    }
+    private val baseUrl = s"${host}/rest/volume"
+
+    private def getUrl(end: String) = s"${baseUrl}/end"
+
+    override def list(): RunnableQuery = RunnableQuery(getUrl("list"), auth)
 
     override def create(name: String, args: Iterable[(String, String)] = Iterable.empty): RunnableCommand = {
-      val url = host + "/rest/volume/create"
-
       val params = List(("name", name)) ++ args
 
-      RunnableCommand(url, auth, params)
+      RunnableCommand(getUrl("create"), auth, params)
+    }
+
+    override def mount(name: String, path: String): RunnableCommand = {
+      val params = List(("name", name), ("path", path))
+
+      RunnableCommand(getUrl("mount"), auth, params)
+    }
+
+    override def unmount(name: String, force: Boolean): RunnableCommand = {
+      val params = List(("name", name), ("force", if (force) "1" else "0"))
+
+      RunnableCommand(getUrl("unmount"), auth, params)
     }
   }
 
